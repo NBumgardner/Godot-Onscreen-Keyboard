a05c84ba18ecba939735d94515be1193526fe4b0
Noah Bumgardner <noah@playgamecafe.com>
2026-09-23 21:44:42 -0500

    Fix Most of Keyboard with Migration Guide, Error

--- addons/onscreenkeyboard/keyboard_button.gd
+++ addons/onscreenkeyboard/keyboard_button.gd
@@ -7,14 +7,22 @@ signal down
 
 var icon_tex_rect
 
-var focused = false setget set_focused
-var pressing = false setget set_pressing
+@export var focused:bool = false:
+	set(new_val):
+		focused = new_val
+	get:
+		return focused
+@export var pressing:bool = false:
+	set(new_val):
+		pressing = new_val
+	get:
+		return pressing
 var id_x = 0
 var id_y = 0
 
 func set_focused(_focused):
 	focused = _focused
-	update()
+	queue_redraw()
 
 func set_pressing(_pressing):
 	if pressing != _pressing:
@@ -23,7 +31,7 @@ func set_pressing(_pressing):
 		else:
 			emit_signal("button_up")
 	pressing = _pressing
-	update()
+	queue_redraw()
 
 func _enter_tree():
 	pass
@@ -32,13 +40,13 @@ func _ready():
 	pass # Replace with function body.
 
 func _draw():
-	var style = get_stylebox("normal")
+	var style = get_theme_stylebox("normal")
 	if pressing or get_draw_mode() == DRAW_PRESSED or (toggle_mode and pressed):
-		draw_style_box(get_stylebox("pressed"), Rect2(Vector2.ZERO, rect_size))
+		draw_style_box(get_theme_stylebox("pressed"), Rect2(Vector2.ZERO, rect_size))
 	else:
 		draw_style_box(style, Rect2(Vector2.ZERO, rect_size))
 	if focused:
-		draw_style_box(get_stylebox("focus"), Rect2(Vector2.ZERO, rect_size))
+		draw_style_box(get_theme_stylebox("focus"), Rect2(Vector2.ZERO, rect_size))
 	var font = get_font("font")
 	var text_ofs = ((rect_size - style.get_minimum_size() - font.get_string_size(text)) / 2.0) + style.get_offset();
 	text_ofs.y += font.get_ascent();
